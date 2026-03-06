9b8202372946d32e4915bb075986b3fc728ab848
newjiayou <2096339243@qq.com>
2026-03-06 19:42:25 +0800

    修正

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_videoplayer_FILE C:/Users/86139/Documents/video/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/videoplayer.exe)
-set(__QT_DEPLOY_TARGET_videoplayer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_videoplayer_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Multimedia.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Concurrent.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
